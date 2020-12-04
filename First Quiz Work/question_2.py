987fe6b76d106288f61578acd848934aaf0fa657
Brian Tran <btran0014@gmail.com>
2020-12-03 20:50:40 -0500

    Calculates area and circumference of the circle

--- First Quiz Work/question_2.py
+++ First Quiz Work/question_2.py
@@ -1,4 +1,6 @@
-#import math
-import math
-#ask for the radius of the circle
-radius = int(input("What is the radius?: "))
+"""This program takes the radius value that is entered by the user and figures out and displays the area and circumference of the circle
+"""
+import math #import math module
+radius = int(input("What is the radius?: ")) #ask for the radius of the circle
+print(f"The area is: {round(math.pi*(radius**2),1)}") #area of the circle is printed
+print (f"The circumference is:100 {round(2*math.pi*radius,1)}")#circumference of the circle is printed
