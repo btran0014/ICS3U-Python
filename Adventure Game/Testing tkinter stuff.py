4e6f80a645fa8f5923e69f105d8d8c39e3f7cda4
Brian Tran <btran0014@gmail.com>
2021-01-01 20:12:45 -0500

    Update Testing tkinter stuff.py
    telling the button what to do

--- Adventure Game/Testing tkinter stuff.py
+++ Adventure Game/Testing tkinter stuff.py
@@ -1,10 +1,12 @@
-#import everything from tkinter
 from tkinter import * 
 
-#opens the window (root widget)
 root = Tk() 
 
-myButton = Button(root, text="Click Me!", padx=50, pady=50)
+def myClick():
+    myLabel = Label(root,text="You clicked the button!")
+    myLabel.pack()
+
+myButton = Button(root, text="Click Me!", padx=100, pady=50, command=myClick)
 myButton.pack()
 root.mainloop()
 
