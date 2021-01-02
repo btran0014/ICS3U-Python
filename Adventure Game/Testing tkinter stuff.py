75da740127b0e4253599b12fc874bbaafc85432d
Brian Tran <btran0014@gmail.com>
2021-01-01 20:05:05 -0500

    Update Testing tkinter stuff.py
    playing around with the grid system and learning about it

--- Adventure Game/Testing tkinter stuff.py
+++ Adventure Game/Testing tkinter stuff.py
@@ -1,10 +1,17 @@
 #import everything from tkinter
 from tkinter import * 
+
 #opens the window (root widget)
 root = Tk() 
-#creating a Label Widget
-myLabel = Label(root,text=" This is a test. Hello Brian! This is a basic GUI! ") 
-#putting it on the screen
-myLabel.pack() 
 
+#creating a Label Widget
+myLabel1 = Label(root,text=" This is a test. ") 
+myLabel2 = Label(root,text=" Hello Brian! This is a basic GUI! ") 
+myLabel3 = Label(root,text="              ") 
+
+#putting it on the screen
+myLabel1.grid(row=0, column=0) 
+myLabel2.grid(row=1, column=5) 
+myLabel3.grid(row=1, column=1) 
 root.mainloop()
+
