f8fba2e70c4f5368643fee609a35b267556684a8
Brian Tran <btran0014@gmail.com>
2021-01-01 20:08:27 -0500

    Update Testing tkinter stuff.py
    Making a button! Also learning how to use them!

--- Adventure Game/Testing tkinter stuff.py
+++ Adventure Game/Testing tkinter stuff.py
@@ -4,17 +4,8 @@ from tkinter import *
 #opens the window (root widget)
 root = Tk() 
 
-#creating a Label Widget
-myLabel1 = Label(root,text=" This is a test. ").grid(row=0, column=0)  
-myLabel2 = Label(root,text=" Hello Brian! This is a basic GUI! ").grid(row=1, column=5) 
-myLabel3 = Label(root,text="              ").grid(row=1, column=1) 
-
-#putting it on the screen
-"""
-myLabel1.grid(row=0, column=0) 
-myLabel2.grid(row=1, column=5) 
-myLabel3.grid(row=1, column=1) 
-"""
+myButton = Button(root, text="Click Me!")
+myButton.pack()
 root.mainloop()
 
  
