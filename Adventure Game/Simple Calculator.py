f70ca4e2f6e78fda4fd2ea83d19bece9a25e04de
Brian Tran <btran0014@gmail.com>
2021-01-06 11:10:13 -0500

    Update Simple Calculator.py

--- Adventure Game/Simple Calculator.py
+++ Adventure Game/Simple Calculator.py
@@ -80,25 +80,22 @@ button_subtract = Button(root,text="-", padx=40, pady=20, command=button_subtrac
 button_multiply = Button(root,text="*", padx=39, pady=20, command=button_multiply)
 button_divide = Button(root,text="/", padx=39, pady=20, command=button_divide)
 
-#Put the buttons on the screen
+#Put the buttons on the screen in the right order
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
-
 button_4.grid(row=2, column=0)
 button_5.grid(row=2, column=1)
 button_6.grid(row=2, column=2)
-
 button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
-
 button_0.grid(row=4, column=0)
-
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_subtract.grid(row=6, column=0)
 button_multiply.grid(row=6, column=1)
 button_divide.grid(row=6, column=2)
+
 root.mainloop()
