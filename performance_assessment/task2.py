8ddbcb762c9bf3075362b3b219022ecf7a95e175
Brian Tran <btran0014@gmail.com>
2021-01-19 10:41:51 -0500

    Updated task 2
    Got it to do the sum of all numbers and sum of all digits. Also fixed prime numbers to output the factors if it wasn't prime.

--- performance_assessment/task2.py
+++ performance_assessment/task2.py
@@ -4,24 +4,28 @@ def odd_or_even():
     else:
         print (user_num, "is an even number!")
 
+
 def prime_or_not():
-    prime = True
-    for i in range (2, user_num//2):
-        prime == True
-        if (user_num % i)==0:
-            prime == False 
-            break
-        elif prime == False: 
-            print(user_num, "is not a prime number.")
-            break
-        elif prime == True:
-            print (user_num, "is a prime number")
+    if user_num > 1:
+        for i in range(2,user_num):
+            if (user_num % i) == 0:
+                print(user_num,"is not a prime number")
+                print(i,"times",user_num//i,"is",user_num)
+    print("If nothing is printed, your number is a prime number. Otherwise, the statements above are true.")
 
+def sum_of_all_nums():
+    sumlist = []
+    for i in range (0, user_num+1):
+        sumlist.append(i)
+    print (f"The sum of all numbers leading up to {user_num} is {sum(sumlist)}")
+
+def sum_of_all_digits():
+    usernumlist = [int(i) for i in str(user_num)]
+    print (f"The sum of all digits in {user_num} is {sum(usernumlist)}")
     
     
-
-
-
+            
+            
 
 use_again = input("Would you like to use this program? (Y/N).")
 while use_again == "Y":
@@ -32,7 +36,8 @@ while use_again == "Y":
             user_num = int(input("What is your number?: "))
             odd_or_even()
             prime_or_not()
-
+            sum_of_all_nums()
+            sum_of_all_digits()
         except ValueError:
             print("You must enter a number!")
         use_again = input("Would you like to use this program again? (Y/N).")
